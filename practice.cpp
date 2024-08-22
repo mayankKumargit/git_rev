1ece711bced9fec544f4be5969db3ba4c46b16c3
mayankKumar <kmayankkr@gmail.com>
2024-08-22 23:31:31 +0530

    adding vector to branch1

--- practice.cpp
+++ practice.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 #include<unordered_set>
+#include<vector>
 int main()
 {
     cout<<"Hello world";
@@ -8,4 +9,6 @@ int main()
     unordered_set<int>s;
     s.insert(10);
     s.insert(20);
+    vector<int>v;
+    v.push_back(30);
 }
